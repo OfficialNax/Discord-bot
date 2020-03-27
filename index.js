3f633662f7eda9fa278686716317256f4fa185ae
OfficialNax <46576310+OfficialNax@users.noreply.github.com>
2020-03-27 02:04:55 +0000

    Added Level System
    - Leveling System into server
        - Levels Per Person
        - Amount of messages needed per Level up is changable
    
    - Adding Settings Menu esc command

--- index.js
+++ index.js
@@ -10,15 +10,20 @@ for (const file of commandFiles) {
   commands.set(command.name, command);
 }
 
-const Token = 'YOUR TOKEN'; //Login Token For Bot
+const Token = ''; //Login Token For Bot
 const prefix = '$';
 
-var Version = '1.1.0';
+var Version = '1.1.1';
 var init = true;
 var initPass = 0;
+var MessagesNeeded = 10;
 const sqrap = require('sqrap');
 var CsgoDevChannel = '';
 var updated = false;
+
+var Members = [];
+var levels = [];
+var MessagesSent = [];
 
 const csgoLogo = 'http://media.steampowered.com/apps/csgo/blog/images/fb_image.png?v=5';
 const csgoPageUrl = 'https://blog.counter-strike.net/index.php/category/updates';
@@ -26,30 +31,26 @@ var Page = '';
 var PageAll = '';
 var NewPage = '';
 const selectors = { //Csgo page selectors
-  title: [
-    {
-      selector: 'h2',
-      text: 'true'
-    }
-  ],
-  all: [
-    {
-      selector: 'div[class="inner_post"]',
-      text: 'true'
-    }
-  ],
-  date: [
-    {
-      selector: 'p',
-      text: 'true'
-    }
-  ]
+  title: [{
+    selector: 'h2',
+    text: 'true'
+  }],
+  all: [{
+    selector: 'div[class="inner_post"]',
+    text: 'true'
+  }],
+  date: [{
+    selector: 'p',
+    text: 'true'
+  }]
 };
 
-sqrap(csgoPageUrl, { selectors }).then(result => {
-  console.log(result);
+sqrap(csgoPageUrl, {
+  selectors
+}).then(result => {
+  console.log("Bot - Updated CSGO update log");
   Page = result;
-  PageAll = result.all.substring(Page.title.length + Page.date.length * 2).split('–');  //csgo page parcer
+  PageAll = result.all.substring(Page.title.length + Page.date.length * 2).split('–'); //csgo page parcer
 }).catch(console.log);
 
 Bot.on('ready', () => {
@@ -59,8 +60,10 @@ Bot.on('ready', () => {
 
 Bot.on('message', message => {
 
-  sqrap(csgoPageUrl, { selectors }).then(result => {
-    console.log(result);
+  sqrap(csgoPageUrl, {
+    selectors
+  }).then(result => {
+    console.log("Bot - Updated CSGO update log");
     NewPage = result;
     //PageAll = result.all.substring(Page.title.length + Page.date.length * 2).split('–');  //csgo page parcer
   }).catch(console.log);
@@ -70,11 +73,81 @@ Bot.on('message', message => {
     Page = NewPage;
   }
 
+  var MessageName = message.author.id;
+  if(MessageName === undefined){
+    MessageName = "Bitch Boi";
+  }
+
+  if (message.author.username != "Bitch Boi") {
+
+    if (Members.includes(MessageName)) {
+      var index = Members.indexOf(MessageName);
+      
+      MessagesSent[index] += 1;
+
+      
+
+      if (MessagesSent[index] % MessagesNeeded === 0) {
+        levels[index] += 1;
+        if(levels[index] === 69){
+          var LevelUpEmbed = new Discord.MessageEmbed()
+          .setColor(0xFFFAA)
+          .setThumbnail(message.author.avatarURL)
+          .setTitle(`${message.author.username} Leveled Up!`)
+          .setDescription(`${message.author.username} Leveled Up To Level ${levels[index]} ( ͡° ͜ʖ ͡°)`);
+          message.reply(LevelUpEmbed);
+        }
+        else{
+        var LevelUpEmbed = new Discord.MessageEmbed()
+        .setColor(0xFFFAA)
+        .setThumbnail(message.author.avatarURL)
+        .setTitle(`${message.author.username} Leveled Up!`)
+        .setDescription(`${message.author.username} Leveled Up To Level ${levels[index]}`);
+        message.reply(LevelUpEmbed);
+        console.log(`Bot - Member Leveled Up  -  ${MessageName}  | Level: ${levels[index]}`);
+        }
+      }
+
+    } else {
+      Members.push(MessageName);
+      levels.push(0);
+      MessagesSent.push(0);
+      console.log(`Bot - Added Member: ${MessageName}`);
+    }
+  }
+
   if (!(message.content.charAt(0) === prefix)) return; //Check That String Uses Correct Prefix 
 
   let args = message.content.substring(prefix.length).split(" "); //Splits string into array of strings by ' '
 
   switch (args[0]) { //args[0] means if $ping ip   args[0] = ping, args[1] = ip
+
+
+    case 'settings':
+      if (!message.member.permissions.has('ADMINISTRATOR', true)) return message.channel.send(`${message.author}, You're not an Admin!`);
+      
+      if(!args[1]){
+        var SettingsEmbed = new Discord.MessageEmbed()
+        .setTitle("Settings Menu")
+        .setDescription("Settings Options - Command | Description")
+        .setColor(0xFFFAA)
+        .addField("***$levelupamount***", "Amount of messages Needed to level up", true);
+
+        message.channel.send(SettingsEmbed);
+
+      }
+      else if(args[1] === 'levelupamount'){
+        if(!args[2]){
+          message.reply("Error: Missing New Amount...");
+        }else{
+          MessagesNeeded = args[2];
+          message.reply(`Changed Messages Needed To ${MessagesNeeded}`);
+          console.log("Bot - Setting Changed : Messages Needed To level up");
+        }
+      }
+                    //Add More Settings Options
+      break;
+
 
     case 'kick':
       if (!message.member.permissions.has('ADMINISTRATOR', true)) return message.channel.send(`${message.author}, You're not an Admin!`);
@@ -91,13 +164,17 @@ Bot.on('message', message => {
       break;
 
     case 'botcsgo':
-      sqrap(csgoPageUrl, { selectors }).then(result => {
+      sqrap(csgoPageUrl, {
+        selectors
+      }).then(result => {
         Page = result;
         PageAll = result.all.substring(Page.title.length + Page.date.length * 2).split('–');
       });
 
-      CsgoDevChannel = message.guild.channels.cache.find(CsgoDevChannel => CsgoDevChannel.name === "csgo"); console.log("Channel Found - " + CsgoDevChannel);
-      if (!CsgoDevChannel) CsgoDevChannel = message.guild.channels.cache.find(CsgoDevChannel => CsgoDevChannel.name === "general"); console.log("Channel Found - " + CsgoDevChannel);;
+      CsgoDevChannel = message.guild.channels.cache.find(CsgoDevChannel => CsgoDevChannel.name === "csgo");
+      console.log("Channel Found - " + CsgoDevChannel);
+      if (!CsgoDevChannel) CsgoDevChannel = message.guild.channels.cache.find(CsgoDevChannel => CsgoDevChannel.name === "general");
+      console.log("Channel Found - " + CsgoDevChannel);;
       if (!CsgoDevChannel) return;
 
       const csgoUpdateBot = new Discord.MessageEmbed()
@@ -113,20 +190,23 @@ Bot.on('message', message => {
         if (initPass === 2) {
           init = false;
         }
-      }
-      else {
+      } else {
         CsgoDevChannel.send(csgoUpdateBot);
       }
       break;
 
-    case 'csgo':   //Doesnt work as a command in seperate file, would have to be formatted differently
-      sqrap(csgoPageUrl, { selectors }).then(result => {
+    case 'csgo': //Doesnt work as a command in seperate file, would have to be formatted differently
+      sqrap(csgoPageUrl, {
+        selectors
+      }).then(result => {
         Page = result;
         PageAll = result.all.substring(Page.title.length + Page.date.length * 2).split('–');
       });
 
-      CsgoDevChannel = message.guild.channels.cache.find(CsgoDevChannel => CsgoDevChannel.name === "csgo"); console.log("Channel Found - " + CsgoDevChannel);
-      if (!CsgoDevChannel) CsgoDevChannel = message.guild.channels.cache.find(CsgoDevChannel => CsgoDevChannel.name === "general"); console.log("Channel Found - " + CsgoDevChannel);;
+      CsgoDevChannel = message.guild.channels.cache.find(CsgoDevChannel => CsgoDevChannel.name === "csgo");
+      console.log("Channel Found - " + CsgoDevChannel);
+      if (!CsgoDevChannel) CsgoDevChannel = message.guild.channels.cache.find(CsgoDevChannel => CsgoDevChannel.name === "general");
+      console.log("Channel Found - " + CsgoDevChannel);;
       if (!CsgoDevChannel) return;
 
 
@@ -170,7 +250,7 @@ Bot.on('message', message => {
 
   }
 
-
+  console.log(`Bot - Responded To Message By: ${message.author.username} - Given Command: ${args[0]}`);
 
   /*
   if(msg.content === 'Hello'){
@@ -181,8 +261,10 @@ Bot.on('message', message => {
 
 Bot.on('guildMemberAdd', member => {
 
-  const channel = member.guild.channels.cache.find(channel => channel.name === "welcome"); console.log("Channel Found - " + channel);
-  if (!channel) channel = member.guild.channels.cache.find(channel => channel.name === "general"); console.log("Channel Found - " + channel);;
+  const channel = member.guild.channels.cache.find(channel => channel.name === "welcome");
+  console.log("Channel Found - " + channel);
+  if (!channel) channel = member.guild.channels.cache.find(channel => channel.name === "general");
+  console.log("Channel Found - " + channel);;
   if (!channel) return;
   const embed = new Discord.MessageEmbed()
     .setTitle(`Welcome ${member.displayName} To ${member.guild.name}!`)
@@ -191,7 +273,10 @@ Bot.on('guildMemberAdd', member => {
     .setFooter("Enjoy Your Stay!");
   channel.send(embed);
 
-
+  Members.push(member.id);
+  levels.push(0);
+  MessagesSent.push(0);
+  console.log(`Bot - Added Member ${member.id}`);
 })
 
 Bot.login(Token); //Login To bot on Client With our bot token
